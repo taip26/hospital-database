6bfc531969d3d7d5d70fbf938eafa31bd5889545
taip26 <124913293+taip26@users.noreply.github.com>
2025-04-09 14:55:38 -0400

    Added ways to navigate the different views via main view. Need to add global variables for height/width and SQL connection string.

--- src/main/java/gourd/hospitaldatabase/AdminViewController.java
+++ src/main/java/gourd/hospitaldatabase/AdminViewController.java
@@ -1,4 +1,24 @@
 package gourd.hospitaldatabase;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AdminViewController {
+    public void onBackButtonClick(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/gourd/hospitaldatabase/MainController.java
+++ src/main/java/gourd/hospitaldatabase/MainController.java
@@ -1,7 +1,15 @@
 package gourd.hospitaldatabase;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     private final static String CONNECTION_URL = "jdbc:mysql://localhost:3307/hospital?user=root&password=password";
@@ -19,5 +27,32 @@ public class MainController {
     protected void onCloseConnectionButtonClick() {
         String msg = SQL_Manager.closeConnection();
         connectionMessage.setText(msg);
+    }
+
+    @FXML
+    public void onPatientButtonClick(ActionEvent actionEvent) {
+        switchView(actionEvent, "patient-view.fxml");
+    }
+
+    @FXML
+    public void onStaffButtonClick(ActionEvent actionEvent) {
+        switchView(actionEvent, "staff-view.fxml");
+    }
+
+    @FXML
+    public void onAdminButtonClick(ActionEvent actionEvent) {
+        switchView(actionEvent, "admin-view.fxml");
+    }
+
+    private void switchView(ActionEvent actionEvent, String fxmlFile) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
